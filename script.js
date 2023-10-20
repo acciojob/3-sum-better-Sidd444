f553a32eb86d56ea265ce9161b00e1e03e1fb47e
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-10-20 17:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function threeSum(arr, target) {
 // write your code here
-  let curr=Number.MAX_VALUE,ans=0;
-  arr.sort();
-  for(let i=0;i<arr.length-2;i++){
-	  let l=i+1,r=arr.length-1;
-	  while(l<r){
-		  let sum=arr[i]+arr[l]+ar[r];
-		  if(Math.abs(sum-target)<curr) {
-		   curr=Math.abs(sum-target);
-	       ans=sum;
-		  }
-		   
-		   if(sum<target) l++;
-		  else r--;
-	  }
-  }
-  console.log(ans);
+  let initialdiff=Number.MAX_VALUE
+let ans=-1
+for(i=0;i<=arr.length-3;i++){
+    for(j=i+1;j<=arr.length-2;j++){
+		for(k=j+1;k<=arr.length-1;k++){
+				let sum=arr[i]+arr[j]+arr[k];
+				let diff=Math.abs(sum-target);
+				if(initialdiff>diff){
+					initialdiff=diff;
+					ans=sum;
+						}
+			}
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
